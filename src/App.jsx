be4cb225dbf289d81e2945d40c9f7d8395dfdcd4
Saptarshi Paul <149397021+Sapta-Dev27@users.noreply.github.com>
2025-01-30 22:14:03 +0530

    Project SetUp

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+ 
+
+  return (
+     <div className=" bg-black">
+      <h1 className="text-white text-4xl  ">GDG Hackathon</h1>
+     </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js}', // Adjust this path to include all your files (HTML, JS, etc.)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
