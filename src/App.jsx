332aedd285b3214071cd60a0ba007f01c211fe10
Saptarshi Paul <149397021+Sapta-Dev27@users.noreply.github.com>
2025-01-31 19:01:03 +0530

    Sponsors Pushed

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,21 @@ import Timeline from "./components/timeline/timeline";
 import Tracks from './components/tracks/tracks';
 import Footer from "./components/footer/Footer";
 import About from './components/home/about'
-
+import Sponsors from './components/sponsors/sponsors'
 
 function App() {
   return (
     <div className="App">
-     
+
       <ParticlesComponent id="particles" />
-    
-     
-      <Navbar/>
-      <Landing/>
-      <About/>
-    
-      
+      <Navbar />
+      <Landing />
+      <About />
       <Hero />
       <Timeline />
       <Tracks />
-
+      <Sponsors />
       <Footer />
-    
     </div>
   );
 }
---
+++ src/components/sponsors/sponsors.jsx
@@ -0,0 +1,53 @@
+import { HoverEffect } from "../ui/card-hover-effect";
+
+export default function CardHoverEffectDemo() {
+  return (
+    (<>
+    <div>
+      <h1 className="text-center text-5xl text-white font-bold mt-16 ">Our <span className="text-purple-600"> Sponsors</span></h1>
+    </div>
+    <div className="max-w-5xl mx-auto px-8">
+
+      <HoverEffect items={sponsors} />
+    </div> </>
+    )
+  );
+}
+export const sponsors = [
+  {
+    title: "Devfolio",
+    description:
+      "Platform Sponsor",
+    link: "https://stripe.com",
+  },
+  {
+    title: "Polygon",
+    description:
+      "Gold Sponsor",
+    link: "https://netflix.com",
+  },
+  {
+    title: "EthIndia",
+    description:
+      "Gold Sponsor",
+    link: "https://google.com",
+  },
+  {
+    title: "GDG On Campus TMSL",
+    description:
+      "Venue Partner",
+    link: "https://meta.com",
+  },
+  {
+    title: "Sponsor 1",
+    description:
+      "Description",
+    link: "https://amazon.com",
+  },
+  {
+    title: "Sponsor 2",
+    description:
+      "Description",
+    link: "https://microsoft.com",
+  },
+];
