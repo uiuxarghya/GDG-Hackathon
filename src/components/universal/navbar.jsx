cab4bea21d19934cec84fa2855a802d540e2775f
Saptarshi Paul <149397021+Sapta-Dev27@users.noreply.github.com>
2025-01-31 10:59:40 +0530

    Fixed Issues of Navbar and Landing

--- src/components/universal/navbar.jsx
+++ src/components/universal/navbar.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import Logo from '../../assets/images/logo.png'
+import { IoLogoInstagram } from "react-icons/io5";
+import { FiGithub } from "react-icons/fi";
+import { SiLinkedin } from "react-icons/si";
+import { FaXTwitter } from "react-icons/fa6";
+
 const Navbar = () => {
   
 
@@ -31,7 +36,10 @@ const Navbar = () => {
           </li>
         </div>
         <div className="navbar-auths">
-          <a href="/trueMix" className="btn-2 bg-purple-700">Register Now</a>
+          <a href="" className=" m-2 text-white  hover:text-purple-500 transition-all ease-in"><FiGithub/></a>
+          <a href="" className=" m-2 text-white  hover:text-purple-500 transition-all ease-in"><SiLinkedin/></a>
+          <a href="" className=" m-2 text-white  hover:text-purple-500 transition-all ease-in"><FaXTwitter/></a>
+          <a hrefLang="" className=" m-2 text-white hover:text-purple-500 transition-all ease-in"><IoLogoInstagram/></a>
         </div>
       </div>
     </div>
