304141fb27d06f81b53b5115ea32c85e3a6f7b9e
Saptarshi Paul <149397021+Sapta-Dev27@users.noreply.github.com>
2025-01-31 17:19:12 +0530

    Fixed Issues of About

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,14 @@ import React from "react"
 
 import Landing from './components/home/Landing'
 import Navbar from './components/universal/navbar'
-<<<<<<< HEAD
 import About from './components/home/about'
-import { Route, Router } from "lucide-react"
 
-=======
+
 import Hero from "./components/hero/Hero"
 import ParticlesComponent from "./components/universal/particles"
 import Timeline from "./components/timeline/timeline"
 import Tracks from './components/tracks/tracks'
->>>>>>> 39ac5168a9ebc53ec6b71da83441680f376cdf1d
+
 function App() {
   
 
@@ -22,18 +20,14 @@ function App() {
       <ParticlesComponent id="particles" />
       <Navbar/>
       <Landing/>
-<<<<<<< HEAD
       <About/>
-     </>
-  );
-=======
+    
       
       <Hero />
       <Timeline/>
       <Tracks/>
     </div>
   )
->>>>>>> 39ac5168a9ebc53ec6b71da83441680f376cdf1d
 }
 
 export default App;
