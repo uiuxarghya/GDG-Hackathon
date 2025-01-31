23a705ebf896729a5c2d4ede935ffccf33086282
Saptarshi Paul <149397021+Sapta-Dev27@users.noreply.github.com>
2025-01-31 11:32:53 +0530

    Particles JS Implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from "react"
 import Landing from './components/home/Landing'
 import Navbar from './components/universal/navbar'
-
+import ParticlesComponent from "./components/universal/particles"
 function App() {
   
 
   return (
 
-     <>
+    <div className="App">
+      <ParticlesComponent id="particles" />
       <Navbar/>
       <Landing/>
-     </>
+    </div>
   )
 }
 
